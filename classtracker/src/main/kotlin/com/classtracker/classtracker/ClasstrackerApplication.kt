018379fe9387a54f5a16aa2a48b9084954e3fa24
mimadraza <m.imadraza@gmail.com>
2025-07-05 15:13:39 +0500

    project is setup

---
+++ classtracker/src/main/kotlin/com/classtracker/classtracker/ClasstrackerApplication.kt
@@ -0,0 +1,11 @@
+package com.classtracker.classtracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClasstrackerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ClasstrackerApplication>(*args)
+}
---
+++ classtracker/src/test/kotlin/com/classtracker/classtracker/ClasstrackerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.classtracker.classtracker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ClasstrackerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
