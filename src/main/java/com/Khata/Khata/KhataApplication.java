37454449b1969d387eeb1f6e4de8dac24833ac6f
mimadraza <m.imadraza@gmail.com>
2025-10-04 00:29:36 +0500

    added the structure

---
+++ src/main/java/com/Khata/Khata/KhataApplication.java
@@ -0,0 +1,13 @@
+package com.Khata.Khata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhataApplication.class, args);
+	}
+
+}
