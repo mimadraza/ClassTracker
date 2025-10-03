00812a1979ce2584693695df7798b088692a010d
Muhammad Imad Raza <114289845+mimadraza@users.noreply.github.com>
2025-10-04 00:05:45 +0500

    Delete classtracker directory

--- classtracker/src/main/kotlin/com/classtrackeer/classtracker/Repository/GigRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.classtrackeer.classtracker.Repository
-
-import com.classtrackeer.classtracker.Model.Gig
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface GigRepository : JpaRepository<Gig, Int> {
-}
--- classtracker/src/main/kotlin/com/classtrackeer/classtracker/Repository/UserRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.classtrackeer.classtracker.Repository
-
-import com.classtrackeer.classtracker.Model.User
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserRepository : JpaRepository<User, Int>{
-}
